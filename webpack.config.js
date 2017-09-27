4896bb0846f93cc3a803ba6b0692d2821cc25ac0
Guillaume DUCLOS <guillaume-duclos@hotmail.fr>
2017-09-28 00:18:00 +0200

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const dev = process.env.NODE_ENV === 'dev';
+
+let config = {
+  entry: './src/app.js',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  plugins: []
+}
+
+/*if(!dev) {
+  config.plugins.push(new UglifyJSPlugin())
+}*/
+
+module.exports = config
